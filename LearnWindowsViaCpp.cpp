6a8a698346be9f9e910736346e8523e80a289ae9
ilsao <ilsao@outlook.com>
2024-05-15 23:29:13 +0800

    Learn Chapter 2.5.2 and finished the code

--- LearnWindowsViaCpp.cpp
+++ LearnWindowsViaCpp.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 int main() {
+
+	WCHAR c = TEXT('a');
+
+	WCHAR szZBuffer[3] = L"";
+	wcscpy_s(szZBuffer, L"hi");
+
+	std::cout << szZBuffer;
 
 	return 0;
 }
